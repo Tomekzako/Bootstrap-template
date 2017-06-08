f59b1a9d679f5d12a51d3de36c5594eb3e4a8504
Tomasz Chyc <tomek.chyc55@gmail.com>
2017-06-08 14:24:03 +0200

    animation on mobile and desktop img when scroll

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const laptop = document.querySelector('.laptop');
+    const mobile = document.querySelector('.mobile');
+    const imac = document.querySelector('.imac');
+
+    window.addEventListener('scroll', function () {
+
+        if (window.scrollY >= 350) {
+            laptop.classList.add('animate');
+            mobile.classList.add('animate');
+        } else {
+            laptop.classList.remove('animate');
+            mobile.classList.remove('animate');
+        }
+    });
+});
